c03f3d28c4aab685314398949bd2ba6d2bda8b3c
tnoff <1326564+tnoff@users.noreply.github.com>
2024-11-12 21:31:39 -0800

    Start sdk on server start

--- serveronly/index.js
+++ serveronly/index.js
@@ -1,6 +1,18 @@
 const app = require("../js/app");
 const Log = require("../js/logger");
 
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { ConsoleSpanExporter } from "@opentelemetry/sdk-trace-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+
+const sdk = new NodeSDK({
+	traceExporter: new ConsoleSpanExporter(),
+	instrumentations: [getNodeAutoInstrumentations()]
+});
+
+sdk.start();
+
+
 app.start().then((config) => {
 	const bindAddress = config.address ? config.address : "localhost";
 	const httpType = config.useHttps ? "https" : "http";
