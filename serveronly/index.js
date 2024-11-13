b6c3f3dda124c493d0124e542220858f4030dc6d
tnoff <1326564+tnoff@users.noreply.github.com>
2024-11-12 22:07:54 -0800

    Send traces to otlp endpoints proper

--- serveronly/index.js
+++ serveronly/index.js
@@ -3,10 +3,26 @@ const Log = require("../js/logger");
 
 const { NodeSDK } = require("@opentelemetry/sdk-node");
 const { getNodeAutoInstrumentations } = require("@opentelemetry/auto-instrumentations-node");
-const { ConsoleSpanExporter } = require("@opentelemetry/sdk-trace-node");
+const { OTLPTraceExporter } = require("@opentelemetry/exporter-trace-otlp-http");
+const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
+const { BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
+const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
+const { Resource } = require("@opentelemetry/resources");
+
+const resource = Resource.default().merge(
+	new Resource({
+		[SemanticResourceAttributes.SERVICE_NAME]: "magic-mirror",
+		[SemanticResourceAttributes.SERVICE_VERSION]: "0.0.1"
+	})
+);
+const provider = new NodeTracerProvider({ resource: resource });
+const exporter = new OTLPTraceExporter();
+const processor = new BatchSpanProcessor(exporter);
+provider.addSpanProcessor(processor);
+provider.register();
 
 const sdk = new NodeSDK({
-	traceExporter: new ConsoleSpanExporter(),
+	traceExporter: exporter,
 	instrumentations: [getNodeAutoInstrumentations()]
 });
 
